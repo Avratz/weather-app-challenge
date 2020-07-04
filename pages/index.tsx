6cbd6e4ab0b9300d125074c85af35303217b71df
Avratz <signos97@gmail.com>
2020-07-03 21:41:01 -0300

    Try, catch and checking data is not null.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,10 @@ import axios from 'axios'
 
 import { GetServerSideProps } from 'next'
 export default function Home({ localWeather, geoLocalization }) {
+	//check localWeather is not empty
+	if (Object.keys(localWeather).length === 0) {
+		return null
+	}
 	return (
 		<Layout title={'Weather App'}>
 			<SingleCity weatherToday={localWeather} />
@@ -12,12 +16,34 @@ export default function Home({ localWeather, geoLocalization }) {
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-	const { data: geoLocalization } = await axios.get(
-		'http://ip-api.com/json/?fields=status,message,country,countryCode,city,zip,lat,lon'
-	)
-	const { data: localWeather } = await axios.get(
-		`http://api.openweathermap.org/data/2.5/weather?zip=${geoLocalization.zip},${geoLocalization.countryCode}&units=metric&APPID=${process.env.APP_ID}`
-	)
+	try {
+		let localWeather: { any } | {} = {}
+		let geoLocalization:
+			| {
+					status: string
+					country: string
+					countryCode: string
+					city: string
+					zip: string
+					lat: number
+					lon: number
+			  }
+			| {} = {}
 
-	return { props: { localWeather, geoLocalization } }
+		const { data: getGeoLocalization } = await axios.get(
+			'http://ip-api.com/json/?fields=status,message,country,countryCode,city,zip,lat,lon'
+		)
+		geoLocalization = getGeoLocalization
+
+		//check geoLocalization is not empty
+		if (Object.keys(geoLocalization).length > 0) {
+			const { data: getWeather } = await axios.get(
+				`http://api.openweathermap.org/data/2.5/weather?zip=${geoLocalization['zip']},${geoLocalization['countryCode']}&units=metric&APPID=${process.env.APP_ID}`
+			)
+			localWeather = getWeather
+		}
+		return { props: { localWeather } }
+	} catch (err) {
+		console.error(err)
+	}
 }
